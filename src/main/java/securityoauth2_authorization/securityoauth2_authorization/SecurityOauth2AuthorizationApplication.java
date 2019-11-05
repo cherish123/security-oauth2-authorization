abd391dc917119123d01eb04c89275bf04180957
cherish <3505162798@qq.com>
2019-11-05 18:18:11 +0800

    sprintboot2.0整合oauth2.0

---
+++ src/main/java/securityoauth2_authorization/securityoauth2_authorization/SecurityOauth2AuthorizationApplication.java
@@ -0,0 +1,13 @@
+package securityoauth2_authorization.securityoauth2_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauth2AuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/securityoauth2_authorization/securityoauth2_authorization/SecurityOauth2AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package securityoauth2_authorization.securityoauth2_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauth2AuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
